9f11db81cc495fc9b3544cd6f2087dbfba90adc6
xbklyn <nut.chotiwit@gmail.com>
2021-09-07 12:37:34 +0700

    week5 work added

---
+++ MyWorks/player.js
@@ -0,0 +1,9 @@
+const player  = (fname, lname) => {
+    firstName : fname === undefined ? fname : `unknown`;
+    lastName : lname ? lname : `unknown`;
+    
+    }
+
+    player("Chotiwit" , "Souyan");
+    console.log(player.firstName);
+
